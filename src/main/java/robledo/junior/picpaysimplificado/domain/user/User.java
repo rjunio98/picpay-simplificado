38515d7bac9510f056c2fdbc5297eacdaebc4789
rjunio98 <robledorjunior1@gmail.com>
2023-12-28 21:24:12 -0300

    Add lombok annotations to User class

--- src/main/java/robledo/junior/picpaysimplificado/domain/user/User.java
+++ src/main/java/robledo/junior/picpaysimplificado/domain/user/User.java
@@ -8,14 +8,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity(name = "users")
 @Table(name = "users")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
 
