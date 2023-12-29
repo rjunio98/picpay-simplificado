548fefe4e75fb2a88cb51cf483ab88b7a8ecdc64
rjunio98 <robledorjunior1@gmail.com>
2023-12-29 10:39:01 -0300

    Add @Repository annotation to TransactionRepository

--- src/main/java/robledo/junior/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/robledo/junior/picpaysimplificado/repositories/TransactionRepository.java
@@ -1,9 +1,11 @@
 package robledo.junior.picpaysimplificado.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import robledo.junior.picpaysimplificado.domain.transaction.Transaction;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     
 }
