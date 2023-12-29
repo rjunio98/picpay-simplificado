d0b5cb6da9b39f15bf17e1c9c9dd38ab8b2c6376
rjunio98 <robledorjunior1@gmail.com>
2023-12-28 21:18:53 -0300

    Add annotations and fields to User class

--- src/main/java/robledo/junior/picpaysimplificado/domain/user/User.java
+++ src/main/java/robledo/junior/picpaysimplificado/domain/user/User.java
@@ -1,6 +1,12 @@
 package robledo.junior.picpaysimplificado.domain.user;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,10 +19,18 @@ import lombok.Setter;
 @EqualsAndHashCode(of = "id")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String document;
+
+    @Column(unique = true)
     private String email;
+    
     private String password;
     private UserType userType;
+    private BigDecimal balance;
 }
