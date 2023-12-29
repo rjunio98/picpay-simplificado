5986287285ea553351a2947ac967bc63fa91496a
rjunio98 <robledorjunior1@gmail.com>
2023-12-28 21:24:08 -0300

    Add Transaction entity class

---
+++ src/main/java/robledo/junior/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,44 @@
+package robledo.junior.picpaysimplificado.domain.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import robledo.junior.picpaysimplificado.domain.user.User;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id")
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "payee_id")
+    private User payee;
+
+    private LocalDateTime transactionTime;
+}
