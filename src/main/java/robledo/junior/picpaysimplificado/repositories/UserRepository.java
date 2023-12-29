c274d2bb9e6a6d2efadf23cfb5c6bb1ef4e88f3d
rjunio98 <robledorjunior1@gmail.com>
2023-12-29 10:39:13 -0300

    Add @Repository annotation to UserRepository

--- src/main/java/robledo/junior/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/robledo/junior/picpaysimplificado/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package robledo.junior.picpaysimplificado.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import robledo.junior.picpaysimplificado.domain.user.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
