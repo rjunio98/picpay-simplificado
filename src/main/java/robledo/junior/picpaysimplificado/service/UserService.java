0798b29f7069ff0713efc68f3053eb8cae8d9dff
rjunio98 <robledorjunior1@gmail.com>
2023-12-31 02:23:42 -0300

    Add getAllUsers method to UserService

--- src/main/java/robledo/junior/picpaysimplificado/service/UserService.java
+++ src/main/java/robledo/junior/picpaysimplificado/service/UserService.java
@@ -1,5 +1,7 @@
 package robledo.junior.picpaysimplificado.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,9 @@ public class UserService {
         User newUser = new User(user);
         this.saveUser(newUser);
         return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 }
