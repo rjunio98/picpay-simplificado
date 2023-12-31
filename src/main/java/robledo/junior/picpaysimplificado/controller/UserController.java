86c5a33afc7c38c08a6c95e1a8d927caf862395f
rjunio98 <robledorjunior1@gmail.com>
2023-12-31 02:23:13 -0300

    Add getAllUsers endpoint to UserController

--- src/main/java/robledo/junior/picpaysimplificado/controller/UserController.java
+++ src/main/java/robledo/junior/picpaysimplificado/controller/UserController.java
@@ -1,5 +1,7 @@
 package robledo.junior.picpaysimplificado.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import robledo.junior.picpaysimplificado.domain.user.User;
 import robledo.junior.picpaysimplificado.domain.user.UserDTO;
 import robledo.junior.picpaysimplificado.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/users")
@@ -24,4 +28,11 @@ public class UserController {
         User newUser = service.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        var users = this.service.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+    
 }
