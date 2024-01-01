e7f8577676dd1403b6e8c2bd58a90fd376a78efe
rjunio98 <robledorjunior1@gmail.com>
2024-01-01 14:58:51 -0300

    Add createTransaction endpoint to TransactionController

--- src/main/java/robledo/junior/picpaysimplificado/controller/TransactionController.java
+++ src/main/java/robledo/junior/picpaysimplificado/controller/TransactionController.java
@@ -1,5 +1,29 @@
 package robledo.junior.picpaysimplificado.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import robledo.junior.picpaysimplificado.domain.transaction.Transaction;
+import robledo.junior.picpaysimplificado.domain.transaction.TransactionDTO;
+import robledo.junior.picpaysimplificado.service.TransactionService;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionController {
     
+    @Autowired
+    private TransactionService transactionService;
+
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception{
+
+        var newTransaction = this.transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
+    }
 }
